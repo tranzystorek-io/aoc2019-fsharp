52a0c58f91a841ebd76214853bc1ba98ea9557af
Marcin Puc <marcin.e.puc@gmail.com>
2020-03-18 22:39:33 +0100

    Add solution for day 4 part 1

---
+++ src/days/Day04Part1/Program.fs
@@ -0,0 +1,35 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open FSharpx.Collections
+
+open ArgParse
+
+let parseInput args =
+    let parse = parseLines "Day 4: Secure Container - Part 1"
+    let lines = parse args
+
+    let line = Seq.head lines
+
+    let split = line.Split('-', 2) |> Seq.map int |> Seq.toArray
+    (split.[0], split.[1])
+
+let isValid password =
+    let rec loopCheck found pairs =
+        match pairs with
+        | LazyList.Nil -> found
+        | LazyList.Cons((a, b), tail) ->
+            if a > b then
+                false
+            else
+                loopCheck (found || a.Equals b) tail
+
+    password |> Seq.pairwise |> LazyList.ofSeq |> loopCheck false
+
+[<EntryPoint>]
+let main argv =
+    let (lower, upper) = parseInput argv
+
+    let solution = seq { lower .. upper } |> Seq.map string |> Seq.filter isValid |> Seq.length
+
+    printfn "%i" solution
+    0 // return an integer exit code
