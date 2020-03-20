030de62e21691de75732eed8d193980725229958
Marcin Puc <marcin.e.puc@gmail.com>
2020-03-20 14:05:12 +0100

    Add solution for day 8 part 1

---
+++ src/days/Day08Part1/Program.fs
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System.Globalization
+
+open ArgParse
+
+let parseInput args =
+    let parse = parseLines "Day 8: Space Image Format - Part 1"
+    let lines = parse args
+
+    let line = Seq.head lines
+
+    line |> Seq.map CharUnicodeInfo.GetDecimalDigitValue |> Seq.toList
+
+let getLayers =
+    let w, h = 25, 6
+    Seq.chunkBySize (w * h) >> Seq.map Seq.toList
+
+[<EntryPoint>]
+let main argv =
+    let data = parseInput argv
+
+    let layers = getLayers data
+
+    let seekedLayer = layers |> Seq.minBy (Seq.filter ((=) 0) >> Seq.length)
+
+    let countElems n = Seq.filter ((=) n) >> Seq.length
+    let ones = countElems 1 seekedLayer
+    let twos = countElems 2 seekedLayer
+
+    let solution = ones * twos
+
+    printfn "%i" solution
+    0 // return an integer exit code
